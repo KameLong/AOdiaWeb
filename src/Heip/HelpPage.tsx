b73f59b4ed393c87b4f0bb653cdeec805a7f25b7
kamelong <kamelong.dev@gmail.com>
2025-02-03 08:43:33 +0900

    v0.1.2

--- src/Heip/HelpPage.tsx
+++ src/Heip/HelpPage.tsx
@@ -27,6 +27,15 @@ export function HelpPage({webOuDia}:{webOuDia:WebOuDia}){
                     <ListItem>
                         <div>
                             <Typography variant={'h6'} component="div">
+                                2025/02/03 v0.1.2
+                            </Typography>
+                            <Typography component="div" sx={{ml: 2}}>
+                                種別編集機能を実装
+                            </Typography>
+                        </div>
+                    </ListItem>                    <ListItem>
+                        <div>
+                            <Typography variant={'h6'} component="div">
                                 2025/02/02 v0.1.1
                             </Typography>
                             <Typography component="div" sx={{ml: 2}}>
